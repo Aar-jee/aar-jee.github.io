5f43eef4695da81d90191f41332c0ad212a6a328
Rohit Ghai <rohitghai91@gmail.com>
2020-09-23 22:13:45 +0530

    Add files via upload

---
+++ precache-manifest.21e89cb75944159f8c8f29a16e93f1ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63b886f39dc1fba90ad9a613bf78ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86105d9d59a2d4fbd8f5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "72e11676150d39af9f10",
+    "url": "/static/css/main.39958ae9.chunk.css"
+  },
+  {
+    "revision": "86105d9d59a2d4fbd8f5",
+    "url": "/static/js/2.e07407e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e07407e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e11676150d39af9f10",
+    "url": "/static/js/main.4b2132eb.chunk.js"
+  },
+  {
+    "revision": "cde08292d8e2ad4028dd",
+    "url": "/static/js/runtime-main.9b4a337c.js"
+  }
+]);
